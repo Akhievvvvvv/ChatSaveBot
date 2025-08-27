8fe28bae425fc6eb616e750915c00ad4a53eabf2
Akhievvvvvv <ahievamir5@gmail.com>
2025-08-27 23:36:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 TOKEN = "8253356529:AAG5sClokG30SlhqpP3TNMdl6TajExIE7YU"
-ADMIN_ID = -1002593269045  # ID вашей группы для уведомлений
-BANK_REQUISITES = "💳 89322229930\n🏦 Ozon Bank"
+ADMIN_ID = -1002593269045  # ID группы для уведомлений
+BANK_REQUISITES = "💳 Реквизиты для оплаты:\n89322229930, Ozon Bank"
 
 TARIFFS = {
-    "2 недели": 49,
-    "1 месяц": 89,
-    "2 месяца": 149
+    "2_weeks": 49,   # 2 недели
+    "1_month": 89,   # 1 месяц
+    "2_months": 149  # 2 месяца
 }
 
 FREE_DAYS = 7
 DATA_PATH = "data"
+USERS_FILE = f"{DATA_PATH}/users.json"
