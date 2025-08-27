0573d4d8de0f5a0d4ff6a3088e38a2dbd512287c
Akhievvvvvv <ahievamir5@gmail.com>
2025-08-27 12:28:19 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "8253356529:AAG5sClokG30SlhqpP3TNMdl6TajExIE7YU"
+ADMIN_ID = -1002593269045  # ID вашей группы для уведомлений
+BANK_REQUISITES = "89322229930, Ozon Bank"
+
+TARIFFS = {
+    "2_weeks": 49,
+    "1_month": 89,
+    "2_months": 149
+}
+
+FREE_DAYS = 7
